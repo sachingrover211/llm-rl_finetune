d3074d366c75abb2584cc865e821971c5c04c74a
Yifan Zhou <yfzhou.cs@gmail.com>
2024-12-10 15:25:22 -0700

    Added more base functions for the world

--- world/mountain_car.py
+++ world/mountain_car.py
@@ -5,3 +5,6 @@ import gym
 class MountainCarWorld(BaseWorld):
     def __init__(self):
         super().__init__("MountainCar-v0")
+
+    def run(self):
+        raise NotImplementedError("run method not implemented")
