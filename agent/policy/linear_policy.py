30dd121ac6471b6cad1d99977ead32d223d529db
Yifan Zhou <yfzhou.cs@gmail.com>
2024-12-17 15:39:11 -0700

    Linear policy on pendulum; more RL trials

--- agent/policy/linear_policy.py
+++ agent/policy/linear_policy.py
@@ -1,39 +1,38 @@
 import numpy as np
-from agent.policy.base_polich import Policy
+from agent.policy.base_policy import Policy
 
 
 class LinearPolicy(Policy):
-    def __init__(self, state, actions):
-        super().__init__(state, actions)
+    def __init__(self, dim_states, dim_actions):
+        super().__init__(dim_states, dim_actions)
 
-        self.dim_states = len(states)
-        self.dim_actions = len(actions)
-        # I dont think we need this but saving none the less
-        self.state = state
-        self.actions = actions
+        self.dim_states =dim_states
+        self.dim_actions = dim_actions
 
+        self.weight = np.random.rand(self.dim_states, self.dim_actions)
+        self.bias = np.random.rand(1, self.dim_actions)
 
     def initialize_policy(self):
         self.weight = np.random.rand(self.dim_states, self.dim_actions)
         self.bias = np.random.rand(1, self.dim_actions)
-        
 
     def get_action(self, state):
-        return self.weight*state + self.bias
+        state = state.T
+        return np.matmul(state, self.weight) + self.bias
 
-
-    def __str__(self)
-        output = ""
+    def __str__(self):
+        output = "Weights:\n"
         for w in self.weight:
-            output = "\t".join([str(i) for i in w])
+            output += ", ".join([str(i) for i in w])
             output += "\n"
 
-        output += "\t".join([str(i) for i in b])
+        output += "Bias:\n"
+        for b in self.bias:
+            output += ", ".join([str(i) for i in b])
+            output += "\n"
 
         return output
 
-
-    def update_policy(self, weight):
-        self.weight = weight[:-1]
-        self.bias = weight[-1]
-
+    def update_policy(self, weight_and_bias_list):
+        self.weight = np.array(weight_and_bias_list[:-1])
+        self.bias = np.expand_dims(np.array(weight_and_bias_list[-1]), axis=0)
