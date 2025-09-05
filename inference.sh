fb06c7346e7ff215be59343dcc052c4e497d5f0e
Sachin Grover <sgrover6@asu.edu>
2025-09-05 15:51:20 -0700

    bug fixes for mountain car continuous runner

--- inference.sh
+++ inference.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 4
 #SBATCH -t 1-12:00:00   # time in d-hh:mm:ss
 #SBATCH -G a100:1
-#SBATCH --mem 40G
+#SBATCH --mem 80G
 #SBATCH -p general
 #SBATCH -q public
 #SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
@@ -14,17 +14,18 @@
 # Load required modules for job's environment
 ml mamba
 # Using python, so source activate an appropriate environment
-source activate py313
+source activate llm
 # load cuda and nccl
 ml cuda-12.4.1-gcc-12.1.0 nccl-2.22.3-1-gcc-12.1.0
 
-export SCRATCH="/scratch/yzhou298"
-export CODE_HOME="/home/yzhou298/avashis9/llm-rl_finetune"
-export ACCELERATE_CONFIG="/home/yzhou298/accelerate_config_single_gpu.yaml"
+export SCRATCH="/scratch/sgrover6"
+export CODE_HOME="/home/sgrover6/src/llm-q"
+export ACCELERATE_CONFIG="/home/sgrover6/src/accelerate_config_rl.yaml"
 export CONFIG_PATH="$CODE_HOME/configs/mc_cont_finetune_oss_baseline.yaml"
 export HF_HOME="$SCRATCH/.cache/huggingface/hub/"
 export OPENAI_API_KEY=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
 cd $CODE_HOME
+export CUDA_VISIBLE_DEVICES="0"
 pwd
 #echo $SLURM_JOB_ID
 #echo $MASTER_ADDR
@@ -38,4 +39,4 @@ export CMD="--config_file $ACCELERATE_CONFIG main.py --config $CONFIG_PATH"
 
 echo $CMD
 
-srun $CMD
+accelerate launch $CMD
