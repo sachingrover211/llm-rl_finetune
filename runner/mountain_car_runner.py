2667cbf6533b5f49933421da2fe4f7663aff9cea
Aakashv1000 <aakashv1000@gmail.com>
2025-09-22 16:20:13 -0700

    added discrete mc

--- runner/mountain_car_runner.py
+++ runner/mountain_car_runner.py
@@ -1,10 +1,10 @@
-from world.mountain_car import MountainCarContinuousWorld
+from world.mountain_car import MountainCarWorld
 from agent.mountain_car import MountainCarDiscreteActionAgent
 from runner.training_loop import training_loop
 
 
 def run_training_loop(**kwargs):
     # world_class & agent_class are passed here, everything else comes from YAML
-    return training_loop(world_class=MountainCarContinuousWorld,
+    return training_loop(world_class=MountainCarWorld,
                      agent_class=MountainCarDiscreteActionAgent,
                      **kwargs)
